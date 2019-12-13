46f36c24e7f73e7791927efe8dce35050d61f483
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2019-12-13 16:00:57 -0600

    Populate setup script with required paths

--- ara.opensciencegrid.org/trunk/setup.sh
+++ ara.opensciencegrid.org/trunk/setup.sh
@@ -1,1 +1,26 @@
+#!/bin/sh
 # Setup script for trunk version of ARA software
+
+export ARA_SETUP_DIR="/scratch/fasig/ara.opensciencegrid.org/trunk"
+
+export PLATFORM_DIR="$ARA_SETUP_DIR/build"
+
+export LD_LIBRARY_PATH="$ARA_SETUP_DIR/build/lib:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$ARA_SETUP_DIR/build/lib:$DYLD_LIBRARY_PATH"
+export PATH="$ARA_SETUP_DIR/build/bin:$PATH"
+
+eval 'source "$ARA_SETUP_DIR"/root_build/bin/thisroot.sh'
+
+export SQLITE_ROOT="$ARA_SETUP_DIR/build"
+export GSL_ROOT="$ARA_SETUP_DIR/build"
+#export FFTW_LIBRARIES="$ARA_SETUP_DIR/build"
+export FFTWSYS="$ARA_SETUP_DIR/build"
+
+export BOOST_ROOT="$ARA_SETUP_DIR/build"
+#export BOOST_LIB="$ARA_SETUP_DIR/build/lib"
+#export LD_LIBRARY_PATH="$BOOST_LIB:$LD_LIBRARY_PATH"
+#export DYLD_LIBRARY_PATH="$BOOST_LIB:$DYLD_LIBRARY_PATH"
+
+export ARA_UTIL_INSTALL_DIR="$ARA_SETUP_DIR/build"
+export ARA_ROOT_DIR="$ARA_SETUP_DIR/source/AraRoot"
+
