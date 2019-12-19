92dee450ce69e45b2d6301d6396de6689f357874
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2019-12-19 15:28:19 -0600

    Restructure environment variables in setup script Also add CMAKE_PREFIX_PATH to assist any user compilation

--- ara.opensciencegrid.org/trunk/setup.sh
+++ ara.opensciencegrid.org/trunk/setup.sh
@@ -4,24 +4,23 @@
 # Get the location of this script
 export ARA_SETUP_DIR=$(cd "$(dirname "$0")" && pwd)
 
-export PLATFORM_DIR="$ARA_SETUP_DIR/build"
+export ARA_UTIL_INSTALL_DIR="${ARA_SETUP_DIR%/}/build"
+export ARA_ROOT_DIR="${ARA_SETUP_DIR%/}/source/AraRoot"
 
-export LD_LIBRARY_PATH="$ARA_SETUP_DIR/build/lib:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$ARA_SETUP_DIR/build/lib:$DYLD_LIBRARY_PATH"
-export PATH="$ARA_SETUP_DIR/build/bin:$PATH"
+export LD_LIBRARY_PATH="$ARA_UTIL_INSTALL_DIR/lib:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$ARA_UTIL_INSTALL_DIR/lib:$DYLD_LIBRARY_PATH"
+export PATH="$ARA_UTIL_INSTALL_DIR/bin:$PATH"
 
-source "$ARA_SETUP_DIR"/root_build/bin/thisroot.sh
+source "${ARA_SETUP_DIR%/}/root_build/bin/thisroot.sh"
 
-export SQLITE_ROOT="$ARA_SETUP_DIR/build"
-export GSL_ROOT="$ARA_SETUP_DIR/build"
-#export FFTW_LIBRARIES="$ARA_SETUP_DIR/build"
-export FFTWSYS="$ARA_SETUP_DIR/build"
+export SQLITE_ROOT="$ARA_UTIL_INSTALL_DIR"
+export GSL_ROOT="$ARA_UTIL_INSTALL_DIR"
+#export FFTW_LIBRARIES="$ARA_UTIL_INSTALL_DIR"
+export FFTWSYS="$ARA_UTIL_INSTALL_DIR"
 
-export BOOST_ROOT="$ARA_SETUP_DIR/build"
-#export BOOST_LIB="$ARA_SETUP_DIR/build/lib"
+export BOOST_ROOT="$ARA_UTIL_INSTALL_DIR"
+#export BOOST_LIB="$ARA_UTIL_INSTALL_DIR/lib"
 #export LD_LIBRARY_PATH="$BOOST_LIB:$LD_LIBRARY_PATH"
 #export DYLD_LIBRARY_PATH="$BOOST_LIB:$DYLD_LIBRARY_PATH"
 
-export ARA_UTIL_INSTALL_DIR="$ARA_SETUP_DIR/build"
-export ARA_ROOT_DIR="$ARA_SETUP_DIR/source/AraRoot"
-
+export CMAKE_PREFIX_PATH="$ARA_UTIL_INSTALL_DIR"
