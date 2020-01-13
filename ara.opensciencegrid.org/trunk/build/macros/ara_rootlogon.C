31aaaae3d7ad639e9ddfab0db1946a21b1c267ac
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2020-01-13 14:49:00 -0600

    Add support for ROOT logon script

---
+++ ara.opensciencegrid.org/trunk/build/macros/ara_rootlogon.C
@@ -0,0 +1,19 @@
+{
+	gSystem->Load("libsqlite3.so");
+	gSystem->Load("libfftw3.so");
+	gSystem->Load("libgsl.so");
+	gSystem->Load("libMathMore.so");
+	gSystem->Load("libGeom.so");
+	gSystem->Load("libGraf3d.so");
+	gSystem->Load("libPhysics.so");
+	gSystem->Load("libRootFftwWrapper.so");
+	gSystem->Load("libAraEvent.so");
+	gSystem->Load("libAraConfig.so");
+	gSystem->Load("libAraDisplay.so");
+	gSystem->Load("libAraCorrelator.so");
+	gSystem->AddIncludePath("-I${ARA_UTIL_INSTALL_DIR}/include");
+	gSystem->AddIncludePath("-I${ARA_UTIL_INSTALL_DIR}/lib");
+	gSystem->AddIncludePath("-I FFTtools.h");
+	gROOT->ProcessLine("#include <FFTtools.h>");
+
+}
